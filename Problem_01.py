c34a4acd8a94f29c46a080ca6bec1042232e13be
ManavShrivastava103 <165892889+ManavShrivastava103@users.noreply.github.com>
2024-04-16 17:13:42 +0000

    Question 1 solution

--- Problem_01.py
+++ Problem_01.py
@@ -1,9 +1,6 @@
 #Defining the function to search for availability of a tshirt brand in shop "One3" :
-
-#The function needs no parameter 
-def get_tshirt():
+def get_tshirt(brand_name):
     name_of_user = get_name()
-    brand_name = input("Enter the name of the tshirt brand you want : ")
     #Below is the list of brands available in shop, (high in demand brands should be kept first to reduce the searching time)
     one3_shop_itemlist = ["Zara","Van Heusen","Nike","Arrow","Roadster","Allen Solly","Jack & Jones","Levi's","US Polo"," Calvin Klein","Wrogn","H&M"]
     for i in range(0,len(one3_shop_itemlist)):
@@ -19,4 +16,5 @@ def get_name():
     return user_name
 
 #Using the function to get the tshirt brand
-get_tshirt()
+input_by_user = input("Enter the name of the tshirt brand you want : ")
+get_tshirt(input_by_user)
